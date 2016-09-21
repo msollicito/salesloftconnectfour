c54239c95c3a157ef90ff87da5fdb1f023708050
michelle.sollicito@gmail.com <michelle.sollicito@gmail.com>
2016-09-21 17:35:47 -0400

    MS Connect4 started

---
+++ Connect4_SalesLoft/Connect4/Connect4/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Connect4.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Play Connect4 with SalesLoft by clicking where you wish to place a piece and making a line of four in any direction - vertically, horizontally or diagonally.";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Connect4 from Salesloft is a brand new concept in online games for Sales Freaks.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "To get in touch with us, call us at";
+
+            return View();
+        }
+    }
+}
